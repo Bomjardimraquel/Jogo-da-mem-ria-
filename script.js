18075edcc5b8a5d5e7e70fecb58e65c95cb94e63
Raquel Bomjardim <114450946+Bomjardimraquel@users.noreply.github.com>
2025-02-18 22:15:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const grid = document.querySelector('.grid');
     const resultDisplay = document.getElementById('result');
     const envelope = document.getElementById('envelope');
-    const letter = document.getElementById('letter');
+    const letter = documentletter');
     const qualities = ['Sincero', 'Compreensivo', 'Sensível', 'Afetuoso', 'Espiritual', 'Íntegro'];
     let cards = [...qualities, ...qualities]; // Duplicate the qualities array
     let hasFlippedCard = false;
@@ -97,8 +97,6 @@ function resetGame() {
     document.getElementById('result').classList.add('hidden');
     document.getElementById('letter').classList.add('hidden');
     document.getElementById('envelope').classList.add('hidden');
-    matchedPairs = 0; 
-    createGrid(); // Cria um novo grid
-}
+    matchedPairs = 0;[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/ricardo-cas/pandas/tree/eefd8f3ed9250c15e029b7ae59a24ef9f7ffc4ab/GUIA_MARKDOWN.MD?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/daniel-carlos/jogo-da-memoria/tree/4fa01325afa03dee2dbdf38040c33d78ff429550/README.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/amarcher/secret-word-agents/tree/9d46e2b5873781b9566d64a38fbd6e0492275c7f/src%2Fgame.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "3")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/Aliadelik07/mnm_experiment/tree/c105662cf954307384ff62525c8ede999477e386/html%2Flib%2Futil-2020.2.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "4")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/derycklong/PluralSight-Angular-Tutorial/tree/f841c806af1dc94e9316799fbcee7b3cc1a0c086/node_modules%2Fsocks%2Fbuild%2Fcommon%2Futil.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "5")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/frkntplglu/memory-card-game/tree/387c6156145212f6bc4a31b206a2ad93f5faacbb/app.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "6")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/rjLaudredale/Memory-cardgame/tree/4ba06cd46caba33d71d523874e0ff69cf147a65b/app.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "7")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/rolandoesc/memory-card-game/tree/f743e65ee595bf6137edb72ea4cf9467528e7aae/scripts.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "8")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/ZoomieOS/flip-card/tree/05dff98b028f300c3d69cda635732f57ce7a5106/js%2Fmain.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "9")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/jennyjohnston/midterm/tree/8f3cb132b75cc82ed705331dc4b95d3f376ada76/script.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "10")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/Gil96/game1/tree/441572b79bb521ec195ca278e5581a75f91200a6/game2%2Fscripts.js?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "11")
 
 
